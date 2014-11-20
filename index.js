a3181324dab7f2602d037e591d78f97dac23e0a9
Ryan Clark <ryan.clark.j@gmail.com>
2014-11-20 13:59:38 -0700

    fill in copy operation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var url = require('url');
 var getUri = require('./lib/get-uri');
 var copy = require('./lib/copy');
+var s3urls = require('s3urls');
 
 var tilelive = require('tilelive');
 var Vector = require('tilelive-vector');
@@ -23,13 +24,25 @@ if (process.env.MapboxUploadValidateFonts)
 var mapnik = require('mapnik');
 mapnik.Logger.setSeverity(mapnik.Logger.NONE);
 
-module.exports = function(filepath, s3urlTemplate, jobInfo, callback) {
+module.exports = function(filepath, s3url, jobInfo, callback) {
+  // Make sure the s3url is of type s3://bucket/key
+  s3url = s3urls.convert(s3url, 's3');
+
   getUri(filepath, function(err, uri) {
     if (err) return callback(err);
 
-    var copyTiles = url.parse(uri).protocol === 'serialtiles:' ?
-      copy.serialtiles : copy.tilelive;
+    var copyTiles = (function(protocol) {
+      // customized copy for serialtiles
+      if (protocol === 'serialtiles:') return copy.serialtiles;
 
-    copyTiles(srcUri, s3urlTemplate, jobInfo, callback);
+      // no-op for tm2z, tilejson
+      if (protocol === 'tm2z:' || protocol === 'tilejson:')
+        return function(a, b, c, cb) { cb(); };
+
+      // otherwise tilelive.copy
+      return copy.tilelive;
+    })(url.parse(uri).protocol);
+
+    copyTiles(srcUri, s3url, jobInfo, callback);
   });
 };
