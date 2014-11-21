30085b49c005cc5fbadee3dfbec304528c52aa92
gretacb <carol@mapbox.com>
2014-11-21 17:01:28 -0500

    add report function and clarify withoutprogress flag

--- bin/mapbox-tile-copy.js
+++ bin/mapbox-tile-copy.js
@@ -30,21 +30,67 @@ if (!argv._[0]) {
     console.log('Options:');
     console.log('  --parts=[number]');
     console.log('  --part=[number]');
-    console.log('  --withoutprogress=[boolean]      Default: false');
+    console.log('  --withoutprogress        Shows progress by default');
     process.exit(1);
 }
 
 argv.parts = argv.parts || undefined;
 argv.part = argv.part || undefined;
-argv.withoutprogress = argv.withoutprogress || false;
+argv.withoutprogress = argv.withoutprogress ? true : false;
 
 var srcuri = argv._[0];
 var dsturi = argv._[1] ? argv._[1] : false;
-var jobs;
+var options;
 
-if (argv.part && argv.parts) jobs = {
+if (!argv.withoutprogress) options.progress = report;
+if (argv.part && argv.parts) options.job = {
     total:argv.parts,
     num:argv.part
 }
 
-init(srcuri, dsturi, jobs);
+init(srcuri, dsturi, options);
+
+function report(stats, p) {
+    util.print(util.format('\r\033[K[%s] %s%% %s/%s @ %s/s | ✓ %s □ %s | %s left',
+        pad(formatDuration(process.uptime()), 4, true),
+        pad((p.percentage).toFixed(4), 8, true),
+        pad(formatNumber(p.transferred),6,true),
+        pad(formatNumber(p.length),6,true),
+        pad(formatNumber(p.speed),4,true),
+        formatNumber(stats.done - stats.skipped),
+        formatNumber(stats.skipped),
+        formatDuration(p.eta)
+    ));
+}
+
+function formatDuration(duration) {
+    var seconds = duration % 60;
+    duration -= seconds;
+    var minutes = (duration % 3600) / 60;
+    duration -= minutes * 60;
+    var hours = (duration % 86400) / 3600;
+    duration -= hours * 3600;
+    var days = duration / 86400;
+
+    return (days > 0 ? days + 'd ' : '') +
+        (hours > 0 || days > 0 ? hours + 'h ' : '') +
+        (minutes > 0 || hours > 0 || days > 0 ? minutes + 'm ' : '') +
+        seconds + 's';
+}
+
+function pad(str, len, r) {
+    while (str.length < len) str = r ? ' ' + str : str + ' ';
+    return str;
+}
+
+function formatNumber(num) {
+    num = num || 0;
+    if (num >= 1e6) {
+        return (num / 1e6).toFixed(2) + 'm';
+    } else if (num >= 1e3) {
+        return (num / 1e3).toFixed(1) + 'k';
+    } else {
+        return num.toFixed(0);
+    }
+    return num.join('.');
+}
