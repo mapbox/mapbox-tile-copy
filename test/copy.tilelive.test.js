e747dc53debb71a55998380e06fd548a51b602eb
mapsam <matthews.sam@gmail.com>
2016-09-16 10:06:23 -0700

    derp

--- test/copy.tilelive.test.js
+++ test/copy.tilelive.test.js
@@ -32,11 +32,10 @@ function dsturi(name) {
 }
 
 function tileCount(dst, callback) {
-  var s3 = new AWS.S3();
   var count = 0;
-
   var region = require('url').parse(dst, true).query.region;
   var params = s3urls.fromUrl(dst.replace('{z}/{x}/{y}', ''));
+  params.Prefix = params.Key;
   delete params.Key;
 
   var s3 = new AWS.S3({ region: region });
