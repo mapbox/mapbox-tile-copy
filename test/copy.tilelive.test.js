06c13adb031aafa3591ab593c6f37d90896d986d
mapsam <matthews.sam@gmail.com>
2017-01-19 14:25:21 -0800

    uncomment

--- test/copy.tilelive.test.js
+++ test/copy.tilelive.test.js
@@ -69,249 +69,249 @@ function tileVersion(dst, z, x, y, callback) {
   });
 }
 
-// test('copy mbtiles', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid.mbtiles');
-//   var src = 'mbtiles://' + fixture;
-//   var dst = dsturi('valid.mbtiles');
-//   sinon.spy(tilelive, 'copy');
-//
-//   tileliveCopy(src, dst, {}, function(err) {
-//     t.ifError(err, 'copied');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 21, 'expected number of tiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].type, 'list', 'uses list scheme for mbtiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].retry, undefined, 'passes options.retry to tilelive.copy');
-//       tilelive.copy.restore();
-//
-//       tileVersion(dst, 0, 0, 0, function(err, version) {
-//         t.ifError(err, 'got tile info');
-//         t.equal(version, 2, 'tile is v2');
-//         t.end();
-//       });
-//     });
-//   });
-// });
-//
-// test('copy retry', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid.mbtiles');
-//   var src = 'mbtiles://' + fixture;
-//   var dst = dsturi('retry');
-//   sinon.spy(tilelive, 'copy');
-//
-//   tileliveCopy(src, dst, {retry:5}, function(err) {
-//     t.ifError(err, 'copied');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 21, 'expected number of tiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].type, 'list', 'uses list scheme for mbtiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].retry, 5, 'passes options.retry to tilelive.copy');
-//       tilelive.copy.restore();
-//       t.end();
-//     });
-//   });
-// });
-//
-// test('copy v2 mbtiles', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid-v2.mbtiles');
-//   var src = 'mbtiles://' + fixture;
-//   var dst = dsturi('valid-v2.mbtiles');
-//   sinon.spy(tilelive, 'copy');
-//   sinon.spy(migrationStream, 'migrate');
-//
-//   tileliveCopy(src, dst, {}, function(err) {
-//     t.ifError(err, 'copied');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 21, 'expected number of tiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].type, 'list', 'uses list scheme for mbtiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].retry, undefined, 'passes options.retry to tilelive.copy');
-//       tilelive.copy.restore();
-//
-//       t.equal(migrationStream.migrate.notCalled, true, 'doesn\t migrate a v2 mbtiles file');
-//       migrationStream.migrate.restore();
-//
-//       tileVersion(dst, 0, 0, 0, function(err, version) {
-//         t.ifError(err, 'got tile info');
-//         t.equal(version, 2, 'tile is v2');
-//         t.end();
-//       });
-//     });
-//   });
-// });
-//
-//
-// test('copy omnivore', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid.geojson');
-//   var src = 'omnivore://' + fixture;
-//   var dst = dsturi('valid.geojson');
-//   sinon.spy(tilelive, 'copy');
-//
-//   tileliveCopy(src, dst, { maxzoom: 5 }, function(err) {
-//     t.ifError(err, 'copied');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 27, 'expected number of tiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].type, 'pyramid', 'uses pyramid scheme for geojson');
-//       tilelive.copy.restore();
-//       t.end();
-//     });
-//   });
-// });
-//
-// test('copy omnivore stats', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid.geojson');
-//   var src = 'omnivore://' + fixture;
-//   var dst = dsturi('valid.geojson');
-//   sinon.spy(tilelive, 'copy');
-//
-//   tileliveCopy(src, dst, { maxzoom: 5, stats: true }, function(err, stats) {
-//     t.ifError(err, 'copied');
-//     t.ok(stats, 'has stats');
-//     t.equal(stats.valid.geometryTypes.Polygon, 207, 'Counts polygons');
-//     tilelive.copy.restore();
-//     t.end();
-//   });
-// });
-//
-// test('copy tilejson', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid.tilejson');
-//   var tmp = path.join(os.tmpdir(), crypto.randomBytes(16).toString('hex'));
-//
-//   var onlineTiles = dsturi('online') + '?acl=public-read';
-//   tilelive.copy('omnivore://' + path.resolve(__dirname, 'fixtures', 'valid.geojson'), onlineTiles, {
-//     maxzoom: 5,
-//     type: 'pyramid',
-//     minzoom: 4,
-//     bounds: [
-//       -124.76214599609376,
-//       24.54521942138596,
-//       -66.95780181884764,
-//       49.3717422485226
-//     ],
-//     close: true
-//   }, function(err) {
-//     t.ifError(err);
-//     sinon.spy(tilelive, 'copy');
-//     fs.readFile(fixture, 'utf8', function(err, data) {
-//       t.ifError(err, 'fixture load');
-//       data = JSON.parse(data);
-//       data.tiles = [ s3urls.convert(onlineTiles, 'bucket-in-host') ];
-//       fs.writeFile(tmp, JSON.stringify(data), runCopy);
-//     });
-//   });
-//
-//   function runCopy() {
-//     var src = 'tilejson://' + tmp;
-//     var dst = dsturi('valid.tilejson');
-//     tileliveCopy(src, dst, {}, function(err) {
-//       t.ifError(err, 'copied');
-//       tileCount(dst, function(err, count) {
-//         t.ifError(err, 'counted tiles');
-//         t.equal(count, 27, 'expected number of tiles');
-//
-//         tileVersion(dst, 4, 2, 5, function(err, version) {
-//           t.ifError(err, 'got tile info');
-//           t.equal(version, 2, 'tile is v2');
-//           t.equal(tilelive.copy.getCall(0).args[2].type, 'scanline', 'uses scanline scheme for tilejson');
-//           tilelive.copy.restore();
-//           t.end();
-//         });
-//       });
-//     });
-//   }
-// });
-//
-// test('copy tm2z', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid.tm2z');
-//   var src = 'tm2z://' + fixture;
-//   var dst = dsturi('valid.tm2z');
-//   sinon.spy(tilelive, 'copy');
-//
-//   tileliveCopy(src, dst, { maxzoom: 3 }, function(err) {
-//     t.ifError(err, 'copied');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 85, 'expected number of tiles');
-//       t.equal(tilelive.copy.getCall(0).args[2].type, 'scanline', 'uses scanline scheme for tm2z');
-//       tilelive.copy.restore();
-//       t.end();
-//     });
-//   });
-// });
-//
-// test('copy in parallel', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'valid.mbtiles');
-//   var src = 'mbtiles://' + fixture;
-//   var dst = dsturi('parallel');
-//   tileliveCopy(src, dst, { job: { total: 10, num: 2 } }, function(err) {
-//     t.ifError(err, 'copied');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.ok(count < 21, 'did not render all tiles');
-//       t.end();
-//     });
-//   });
-// });
-//
-// test('copy invalid source', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'invalid.tilejson');
-//   var src = 'tilejson://' + fixture;
-//   var dst = dsturi('invalid.tilejson');
-//   tileliveCopy(src, dst, {}, function(err) {
-//     t.ok(err, 'expected error');
-//     t.equal(err.code, 'EINVALID', 'marked invalid when cannot load source');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 0, 'rendered no tiles');
-//       t.end();
-//     });
-//   });
-// });
-//
-// test('copy corrupt mbtiles', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'invalid.corrupt.mbtiles');
-//   var src = 'mbtiles://' + fixture;
-//   var dst = dsturi('invalid.mbtiles');
-//   tileliveCopy(src, dst, {}, function(err) {
-//     t.ok(err, 'expected error');
-//     t.equal(err.code, 'SQLITE_CORRUPT', 'pass-through errors encountered during copy');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 0, 'did not render any tiles');
-//       t.end();
-//     });
-//   });
-// });
-//
-// test('passes through invalid tile in mbtiles', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'invalid.tile-with-no-geometry.mbtiles');
-//   var src = 'mbtiles://' + fixture;
-//   var dst = dsturi('invalid.tile-with-no-geometry.mbtiles');
-//   tileliveCopy(src, dst, {}, function(err, stats) {
-//     t.ifError(err, 'passes through invalid.tile-with-no-geometry.mbtiles');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 1, 'rendered all tiles');
-//       t.end();
-//     });
-//   });
-// });
-//
-// test('copy null-tile mbtiles', function(t) {
-//   var fixture = path.resolve(__dirname, 'fixtures', 'invalid.null-tile.mbtiles');
-//   var src = 'mbtiles://' + fixture;
-//   var dst = dsturi('invalid.mbtiles');
-//   tileliveCopy(src, dst, {}, function(err) {
-//     t.ok(err, 'expected error');
-//     t.equal(err.code, 'EINVALIDTILE', 'pass-through errors encountered during copy');
-//     tileCount(dst, function(err, count) {
-//       t.ifError(err, 'counted tiles');
-//       t.equal(count, 0, 'did not render any tiles');
-//       t.end();
-//     });
-//   });
-// });
+test('copy mbtiles', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid.mbtiles');
+  var src = 'mbtiles://' + fixture;
+  var dst = dsturi('valid.mbtiles');
+  sinon.spy(tilelive, 'copy');
+
+  tileliveCopy(src, dst, {}, function(err) {
+    t.ifError(err, 'copied');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 21, 'expected number of tiles');
+      t.equal(tilelive.copy.getCall(0).args[2].type, 'list', 'uses list scheme for mbtiles');
+      t.equal(tilelive.copy.getCall(0).args[2].retry, undefined, 'passes options.retry to tilelive.copy');
+      tilelive.copy.restore();
+
+      tileVersion(dst, 0, 0, 0, function(err, version) {
+        t.ifError(err, 'got tile info');
+        t.equal(version, 2, 'tile is v2');
+        t.end();
+      });
+    });
+  });
+});
+
+test('copy retry', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid.mbtiles');
+  var src = 'mbtiles://' + fixture;
+  var dst = dsturi('retry');
+  sinon.spy(tilelive, 'copy');
+
+  tileliveCopy(src, dst, {retry:5}, function(err) {
+    t.ifError(err, 'copied');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 21, 'expected number of tiles');
+      t.equal(tilelive.copy.getCall(0).args[2].type, 'list', 'uses list scheme for mbtiles');
+      t.equal(tilelive.copy.getCall(0).args[2].retry, 5, 'passes options.retry to tilelive.copy');
+      tilelive.copy.restore();
+      t.end();
+    });
+  });
+});
+
+test('copy v2 mbtiles', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid-v2.mbtiles');
+  var src = 'mbtiles://' + fixture;
+  var dst = dsturi('valid-v2.mbtiles');
+  sinon.spy(tilelive, 'copy');
+  sinon.spy(migrationStream, 'migrate');
+
+  tileliveCopy(src, dst, {}, function(err) {
+    t.ifError(err, 'copied');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 21, 'expected number of tiles');
+      t.equal(tilelive.copy.getCall(0).args[2].type, 'list', 'uses list scheme for mbtiles');
+      t.equal(tilelive.copy.getCall(0).args[2].retry, undefined, 'passes options.retry to tilelive.copy');
+      tilelive.copy.restore();
+
+      t.equal(migrationStream.migrate.notCalled, true, 'doesn\t migrate a v2 mbtiles file');
+      migrationStream.migrate.restore();
+
+      tileVersion(dst, 0, 0, 0, function(err, version) {
+        t.ifError(err, 'got tile info');
+        t.equal(version, 2, 'tile is v2');
+        t.end();
+      });
+    });
+  });
+});
+
+
+test('copy omnivore', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid.geojson');
+  var src = 'omnivore://' + fixture;
+  var dst = dsturi('valid.geojson');
+  sinon.spy(tilelive, 'copy');
+
+  tileliveCopy(src, dst, { maxzoom: 5 }, function(err) {
+    t.ifError(err, 'copied');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 27, 'expected number of tiles');
+      t.equal(tilelive.copy.getCall(0).args[2].type, 'pyramid', 'uses pyramid scheme for geojson');
+      tilelive.copy.restore();
+      t.end();
+    });
+  });
+});
+
+test('copy omnivore stats', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid.geojson');
+  var src = 'omnivore://' + fixture;
+  var dst = dsturi('valid.geojson');
+  sinon.spy(tilelive, 'copy');
+
+  tileliveCopy(src, dst, { maxzoom: 5, stats: true }, function(err, stats) {
+    t.ifError(err, 'copied');
+    t.ok(stats, 'has stats');
+    t.equal(stats.valid.geometryTypes.Polygon, 207, 'Counts polygons');
+    tilelive.copy.restore();
+    t.end();
+  });
+});
+
+test('copy tilejson', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid.tilejson');
+  var tmp = path.join(os.tmpdir(), crypto.randomBytes(16).toString('hex'));
+
+  var onlineTiles = dsturi('online') + '?acl=public-read';
+  tilelive.copy('omnivore://' + path.resolve(__dirname, 'fixtures', 'valid.geojson'), onlineTiles, {
+    maxzoom: 5,
+    type: 'pyramid',
+    minzoom: 4,
+    bounds: [
+      -124.76214599609376,
+      24.54521942138596,
+      -66.95780181884764,
+      49.3717422485226
+    ],
+    close: true
+  }, function(err) {
+    t.ifError(err);
+    sinon.spy(tilelive, 'copy');
+    fs.readFile(fixture, 'utf8', function(err, data) {
+      t.ifError(err, 'fixture load');
+      data = JSON.parse(data);
+      data.tiles = [ s3urls.convert(onlineTiles, 'bucket-in-host') ];
+      fs.writeFile(tmp, JSON.stringify(data), runCopy);
+    });
+  });
+
+  function runCopy() {
+    var src = 'tilejson://' + tmp;
+    var dst = dsturi('valid.tilejson');
+    tileliveCopy(src, dst, {}, function(err) {
+      t.ifError(err, 'copied');
+      tileCount(dst, function(err, count) {
+        t.ifError(err, 'counted tiles');
+        t.equal(count, 27, 'expected number of tiles');
+
+        tileVersion(dst, 4, 2, 5, function(err, version) {
+          t.ifError(err, 'got tile info');
+          t.equal(version, 2, 'tile is v2');
+          t.equal(tilelive.copy.getCall(0).args[2].type, 'scanline', 'uses scanline scheme for tilejson');
+          tilelive.copy.restore();
+          t.end();
+        });
+      });
+    });
+  }
+});
+
+test('copy tm2z', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid.tm2z');
+  var src = 'tm2z://' + fixture;
+  var dst = dsturi('valid.tm2z');
+  sinon.spy(tilelive, 'copy');
+
+  tileliveCopy(src, dst, { maxzoom: 3 }, function(err) {
+    t.ifError(err, 'copied');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 85, 'expected number of tiles');
+      t.equal(tilelive.copy.getCall(0).args[2].type, 'scanline', 'uses scanline scheme for tm2z');
+      tilelive.copy.restore();
+      t.end();
+    });
+  });
+});
+
+test('copy in parallel', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'valid.mbtiles');
+  var src = 'mbtiles://' + fixture;
+  var dst = dsturi('parallel');
+  tileliveCopy(src, dst, { job: { total: 10, num: 2 } }, function(err) {
+    t.ifError(err, 'copied');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.ok(count < 21, 'did not render all tiles');
+      t.end();
+    });
+  });
+});
+
+test('copy invalid source', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'invalid.tilejson');
+  var src = 'tilejson://' + fixture;
+  var dst = dsturi('invalid.tilejson');
+  tileliveCopy(src, dst, {}, function(err) {
+    t.ok(err, 'expected error');
+    t.equal(err.code, 'EINVALID', 'marked invalid when cannot load source');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 0, 'rendered no tiles');
+      t.end();
+    });
+  });
+});
+
+test('copy corrupt mbtiles', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'invalid.corrupt.mbtiles');
+  var src = 'mbtiles://' + fixture;
+  var dst = dsturi('invalid.mbtiles');
+  tileliveCopy(src, dst, {}, function(err) {
+    t.ok(err, 'expected error');
+    t.equal(err.code, 'SQLITE_CORRUPT', 'pass-through errors encountered during copy');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 0, 'did not render any tiles');
+      t.end();
+    });
+  });
+});
+
+test('passes through invalid tile in mbtiles', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'invalid.tile-with-no-geometry.mbtiles');
+  var src = 'mbtiles://' + fixture;
+  var dst = dsturi('invalid.tile-with-no-geometry.mbtiles');
+  tileliveCopy(src, dst, {}, function(err, stats) {
+    t.ifError(err, 'passes through invalid.tile-with-no-geometry.mbtiles');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 1, 'rendered all tiles');
+      t.end();
+    });
+  });
+});
+
+test('copy null-tile mbtiles', function(t) {
+  var fixture = path.resolve(__dirname, 'fixtures', 'invalid.null-tile.mbtiles');
+  var src = 'mbtiles://' + fixture;
+  var dst = dsturi('invalid.mbtiles');
+  tileliveCopy(src, dst, {}, function(err) {
+    t.ok(err, 'expected error');
+    t.equal(err.code, 'EINVALIDTILE', 'pass-through errors encountered during copy');
+    tileCount(dst, function(err, count) {
+      t.ifError(err, 'counted tiles');
+      t.equal(count, 0, 'did not render any tiles');
+      t.end();
+    });
+  });
+});
 
 test('copy coordinates exceed spherical mercator', function(t) {
   var fixture = path.resolve(__dirname, 'fixtures', 'invalid.coords-out-of-range.geojson');
