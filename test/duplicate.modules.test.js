bcc2f5f8c9e944d51d2fb021359e7bcca10c0e0f
gretacb <carol@mapbox.com>
2018-01-03 14:46:08 -0600

    Fix dupe test for updated npm ls output

--- test/duplicate.modules.test.js
+++ test/duplicate.modules.test.js
@@ -7,7 +7,14 @@ var test = require('tape').test;
     exec(cmd, function (error, stdout, stderr) {
       var pattern = new RegExp(mod + '@','g');
       var match = stdout.match(pattern);
-      t.ok(match && match.length === 1, 'one copy of ' + mod + ' (`npm ls ' + mod + '`)');
+      var pattern_dedupe = new RegExp('deduped','g');
+      var deduped = stdout.match(pattern_dedupe);
+
+      if (deduped) {
+      	t.ok(match && deduped && (match.length - 1 === deduped.length), 'one copy of ' + mod + ' (`npm ls ' + mod + '`)');
+      } else {
+      	t.ok(match && match.length === 1, 'one copy of ' + mod + ' (`npm ls ' + mod + '`)');
+      }
       t.end();
     });
   });
