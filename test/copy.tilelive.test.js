86affaef2556b583c4271713e71145d67b51155b
Dane Springmeyer <dane@mapbox.com>
2018-01-05 16:59:29 -0800

    revert changes

--- test/copy.tilelive.test.js
+++ test/copy.tilelive.test.js
@@ -39,7 +39,6 @@ function tileCount(dst, callback) {
   var params = s3urls.fromUrl(dst.replace('{z}/{x}/{y}', ''));
   params.Prefix = params.Key;
   delete params.Key;
-  console.log(JSON.stringify(params));
 
   var s3 = new AWS.S3({ region: region });
 
